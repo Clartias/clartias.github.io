af5ccd5f31cfe2b87cccd4c399dbc465d01cb932
Wei Jian Wong <Jiantastic@users.noreply.github.com>
2023-11-28 15:03:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,53 +14,52 @@ document.getElementById('dropArea').addEventListener('drop', (event) => {
     event.preventDefault();
 
     var files = event.dataTransfer.files;
-
     if (files.length > 0) {
         var file = files[0];
         document.getElementById('fileName').textContent = file.name;
         document.getElementById('fileInfo').style.display = 'block';
-        storeFileClientSide(file);
+
+        // Initiate upload
+        uploadFile(file);
     }
 
     document.getElementById('dropArea').classList.remove('drag-over');
 });
 
-document.getElementById('videoFile').addEventListener('change', function(event) {
+document.getElementById('videoFile').addEventListener('change', function (event) {
     if (this.files.length > 0) {
         var file = this.files[0];
         document.getElementById('fileName').textContent = file.name;
         document.getElementById('fileInfo').style.display = 'block';
+
+        // Initiate upload
+        uploadFile(file);
     }
 });
 
-document.getElementById('cancelButton').addEventListener('click', function() {
+document.getElementById('cancelButton').addEventListener('click', function () {
     document.getElementById('videoFile').value = '';
     document.getElementById('fileInfo').style.display = 'none';
 });
 
-function storeFileClientSide(file) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        console.log('File stored client-side (simulated)');
-    };
-    reader.readAsDataURL(file);
+function getPresignedUrl(file) {
+    return fetch(`https://wrcqcwfqbb.execute-api.us-east-1.amazonaws.com/uploads?filename=${encodeURIComponent(file.name)}&filetype=${encodeURIComponent(file.type)}`)
+        .then(response => response.json())
+        .then(data => data.uploadURL);
 }
 
-function uploadVideo() {
-    var fileInput = document.getElementById('videoFile');
-    if (fileInput.files.length > 0) {
-        var file = fileInput.files[0];
+function uploadFile(file) {
+    getPresignedUrl(file).then(uploadUrl => {
         var xhr = new XMLHttpRequest();
-
         xhr.upload.addEventListener("progress", updateProgress, false);
         xhr.addEventListener("load", transferComplete, false);
 
-        xhr.open("PUT", "https://wrcqcwfqbb.execute-api.us-east-1.amazonaws.com/uploads");
-        xhr.setRequestHeader("Content-Type", "image/jpeg");
+        xhr.open("PUT", uploadUrl);
+        xhr.setRequestHeader("Content-Type", file.type);
         xhr.send(file);
-    } else {
-        alert('Please select or drop a file to upload.');
-    }
+    }).catch(error => {
+        console.error("Error getting a signed URL or uploading the file:", error);
+    });
 }
 
 function updateProgress(e) {
