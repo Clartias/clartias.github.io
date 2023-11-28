f6803734937e04575d8bdb8872562a31fcc1ae37
Wei Jian Wong <Jiantastic@users.noreply.github.com>
2023-11-28 15:59:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let selectedFile;
+
 document.getElementById('dropArea').addEventListener('dragover', (event) => {
     event.stopPropagation();
     event.preventDefault();
@@ -15,53 +17,60 @@ document.getElementById('dropArea').addEventListener('drop', (event) => {
     document.getElementById('dropArea').classList.remove('drag-over');
 
     var files = event.dataTransfer.files;
-    if (files.length > 0) {
-        var file = files[0];
-
-        if (!isFileSizeValid(file)) {
-            alert('File size exceeds the 1GB limit.');
-            return;
-        }
-
-        document.getElementById('fileName').textContent = file.name;
-        document.getElementById('fileInfo').style.display = 'block';
-        uploadFile(file);
-    }
+    handleFileSelection(files);
 });
 
 document.getElementById('videoFile').addEventListener('change', function (event) {
-    if (this.files.length > 0) {
-        var file = this.files[0];
+    handleFileSelection(this.files);
+});
 
-        if (!isFileSizeValid(file)) {
+document.getElementById('cancelButton').addEventListener('click', function () {
+    resetForm();
+});
+
+document.getElementById('submitButton').addEventListener('click', function () {
+    const email = document.getElementById('emailField').value;
+    if (email && selectedFile) {
+        uploadFile(selectedFile, email);
+    } else {
+        alert('Please select a file and enter your email address.');
+    }
+});
+
+function handleFileSelection(files) {
+    if (files.length > 0) {
+        selectedFile = files[0];
+
+        if (!isFileSizeValid(selectedFile)) {
             alert('File size exceeds the 1GB limit.');
             return;
         }
 
-        document.getElementById('fileName').textContent = file.name;
+        document.getElementById('fileName').textContent = selectedFile.name;
         document.getElementById('fileInfo').style.display = 'block';
-        uploadFile(file);
     }
-});
+}
 
-document.getElementById('cancelButton').addEventListener('click', function () {
+function resetForm() {
     document.getElementById('videoFile').value = '';
     document.getElementById('fileInfo').style.display = 'none';
-});
+    selectedFile = null;
+}
 
 function isFileSizeValid(file) {
     const MAX_SIZE = 1024 * 1024 * 1024; // 1GB in bytes
     return file.size <= MAX_SIZE;
 }
 
-function getPresignedUrl(file) {
-    return fetch(`https://wrcqcwfqbb.execute-api.us-east-1.amazonaws.com/uploads?filename=${encodeURIComponent(file.name)}&filetype=${encodeURIComponent(file.type)}`)
+function getPresignedUrl(file, email) {
+    const filename = encodeURIComponent(email + '-' + file.name);
+    return fetch(`https://wrcqcwfqbb.execute-api.us-east-1.amazonaws.com/uploads?filename=${filename}&filetype=${encodeURIComponent(file.type)}`)
         .then(response => response.json())
         .then(data => data.uploadURL);
 }
 
-function uploadFile(file) {
-    getPresignedUrl(file).then(uploadUrl => {
+function uploadFile(file, email) {
+    getPresignedUrl(file, email).then(uploadUrl => {
         var xhr = new XMLHttpRequest();
         xhr.upload.addEventListener("progress", updateProgress, false);
         xhr.addEventListener("load", transferComplete, false);
@@ -84,4 +93,5 @@ function updateProgress(e) {
 
 function transferComplete(e) {
     alert("Upload complete.");
+    resetForm();
 }
